395f2f1d61efd5eae507b458db7ec8ff24c8b716
sharonkorir <sharon.korir@student.moringaschool.com>
2022-04-08 22:55:32 +0300

    database: create postgresql db and connect app

--- web_awards/settings.py
+++ web_awards/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'web_awards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'web_awards',
+        'USER': 'sharon',
+        'PASSWORD': '12345678'
     }
 }
 
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
