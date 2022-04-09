5a40cbc37cb8bd19ccbe6467a591082aa09e68d3
sharonkorir <sharon.korir@student.moringaschool.com>
2022-04-09 12:36:33 +0300

    authentication: add email and names to user registration form

--- users/forms.py
+++ users/forms.py
@@ -3,10 +3,16 @@ from django.contrib.auth.models import User
 from django import forms
 
 class RegisterUserForm(UserCreationForm):
-    email = forms.EmailField()
-    first_name = forms.CharField(max_length=50)
-    last_name = forms.CharField(max_length=50)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'input-field col s12'}))
+    first_name = forms.CharField(max_length=50,widget=forms.TextInput(attrs={'class':'input-field col s12'}))
+    last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'input-field col s12'}))
 
     class Meta:
         model = User
-        fields = ('username', 'first_name','last_name', 'email', 'password1', 'password2')
+        fields = ( 'first_name','last_name', 'email','username', 'password1', 'password2')
+    def __init__(self, *args, **kwargs):
+        super(RegisterUserForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'input-field col s12'
+        self.fields['password1'].widget.attrs['class'] = 'input-field col s12'
+        self.fields['password2'].widget.attrs['class'] = 'input-field col s12'
